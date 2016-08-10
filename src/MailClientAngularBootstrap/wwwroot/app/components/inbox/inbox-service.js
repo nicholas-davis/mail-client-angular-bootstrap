ab11d41ea28ade997d7919edfc0387753a322312
Nick Davis <hire@nicholasdavis.co>
2016-08-10 18:27:21 -0400

    Change factory structure

--- src/MailClientAngularBootstrap/wwwroot/app/components/inbox/inbox-service.js
+++ src/MailClientAngularBootstrap/wwwroot/app/components/inbox/inbox-service.js
@@ -1,15 +1,12 @@
 ﻿//InboxService
 inboxModule.factory('InboxService', ['config', '$http',
     function (config, $http) {
-
-        var inboxData = {};
-
-        inboxData.getInboxMail = function () {
-            return $http.get(config.apiUrl + '/inbox.json').then(function (response) {
-                return response.data;
-            });
+        return {
+            getInboxMail: function () {
+                return $http.get(config.apiUrl + '/inbox.json').then(function (response) {
+                    return response.data;
+                });
+            }
         };
-
-        return inboxData;
     }
 ]);
--- src/MailClientAngularBootstrap/wwwroot/app/components/user/user-service.js
+++ src/MailClientAngularBootstrap/wwwroot/app/components/user/user-service.js
@@ -1,15 +1,12 @@
 ﻿//UserService
 mailModule.factory('UserService', ['config', '$http',
     function (config, $http) {
-
-        var userData = {};
-
-        userData.getUser = function () {
-            return $http.get(config.apiUrl + '/user.json').then(function (response) {
-                return response.data;
-            });
+        return {
+            getUser: function () {
+                return $http.get(config.apiUrl + '/user.json').then(function (response) {
+                    return response.data;
+                });
+            }
         };
-
-        return userData;
     }
 ]);
