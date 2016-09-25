8d799d7dc57ca067eafba68b8291906270e36903
Nick Davis <hire@nicholasdavis.co>
2016-09-25 12:36:37 -0400

    Added conditions for muti email addesses, required and disabled

--- src/MailClientAngularBootstrap/wwwroot/app/shared/validate/validate-directive.js
+++ src/MailClientAngularBootstrap/wwwroot/app/shared/validate/validate-directive.js
@@ -1,11 +1,11 @@
 ﻿//ValidateDirective
-mailModule.directive('validate', [
-    function () {
+mailModule.directive('validate', ['$compile',
+    function ($compile) {
         return {
             restrict: 'E',
             scope: {},
-            templateUrl: 'app/shared/validate/validate-view.html', 
-            controller: 'validateController', 
+            templateUrl: 'app/shared/validate/validate-view.html',
+            controller: 'validateController',
             controllerAs: 'vmValidate',
             bindToController: {
                 form: '=',
@@ -14,10 +14,28 @@ mailModule.directive('validate', [
                 elementClass: '@',
                 labelClass: '@',
                 labelText: '@',
-                placeholder: '@'
+                placeholder: '@',
+                isRequired: '@',
+                isDisabled: '@',
+                isEmail: '@',
             },
-            link: function (scope, element, attrs) {
+            link: function (scope, element, attrs, vmValidate) {
+                console.log(vmValidate)
 
+                //Set field to required
+                if (vmValidate.isRequired) {
+                    scope.isRequired = vmValidate.isRequired;
+                }
+
+                //Set field to disabled
+                if (vmValidate.isDisabled) {
+                    scope.isDisabled = vmValidate.isDisabled;
+                }
+
+                //Check email address and/or email address by separated comma values
+                if (vmValidate.isEmail) {
+                    scope.isEmail = /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4}[\W]*,{1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]{2,4})[\W]*$/;
+                }
             }
         };
     }
