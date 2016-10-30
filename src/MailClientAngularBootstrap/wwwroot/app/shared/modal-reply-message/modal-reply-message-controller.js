6055b462b5b72b3ad3819a6bcf4e669a7806b4a3
Nick Davis <hire@nicholasdavis.co>
2016-10-30 15:20:11 -0400

    Moved content-editable directive into a file

--- src/MailClientAngularBootstrap/wwwroot/app/shared/modal-reply-message/modal-reply-message-controller.js
+++ src/MailClientAngularBootstrap/wwwroot/app/shared/modal-reply-message/modal-reply-message-controller.js
@@ -1,12 +1,12 @@
 ﻿//ModalReplyMessageController
 mailModule.controller('modalReplyMessageController', ['$scope', '$timeout', '$uibModalInstance', 'modalConfig', 'AlertService',
     function ($scope, $timeout, $uibModalInstance, modalConfig, AlertService) {
-        
+
         //Build form
         $scope.formEntry = {
             to: modalConfig.instanceData.from.email,
             from: modalConfig.instanceData.to.email,
-            subject: modalConfig.instanceData.subject,
+            subject: 'Re: ' + modalConfig.instanceData.subject,
             message: '<br /><br /><hr />' + modalConfig.instanceData.message
         };
 
@@ -18,7 +18,7 @@ mailModule.controller('modalReplyMessageController', ['$scope', '$timeout', '$ui
             btnActionText: modalConfig.btnActionText,
             btnCancelText: modalConfig.btnCancelText,
             action: function () {
-                console.log($scope.formEntry)
+
                 //Check subject line
                 if ($scope.formEntry.subject) {
                     //Show confirmed alert
@@ -38,9 +38,6 @@ mailModule.controller('modalReplyMessageController', ['$scope', '$timeout', '$ui
             }
         };
 
-        //Subject line
-        $scope.modal.instanceData.subject = 'Re: ' + $scope.modal.instanceData.subject;
-
         $scope.isLoading = true;
         $uibModalInstance.rendered.then(function () {
             $timeout(function () {
@@ -49,35 +46,4 @@ mailModule.controller('modalReplyMessageController', ['$scope', '$timeout', '$ui
         });
 
     }
-]).directive('contenteditable', function () {
-      return {
-          restrict: 'A', // only activate on element attribute
-          require: '?ngModel', // get a hold of NgModelController
-          link: function (scope, element, attrs, ngModel) {
-              if (!ngModel) return; // do nothing if no ng-model
-
-              // Specify how UI should be updated
-              ngModel.$render = function () {
-                  element.html(ngModel.$viewValue || '');
-              };
-
-              // Listen for change events to enable binding
-              element.on('blur keyup change', function () {
-                  scope.$apply(readViewText);
-              });
-
-              // No need to initialize, AngularJS will initialize the text based on ng-model attribute
-
-              // Write data to the model
-              function readViewText() {
-                  var html = element.html();
-                  // When we clear the content editable the browser leaves a <br> behind
-                  // If strip-br attribute is provided then we strip this out
-                  if (attrs.stripBr && html == '<br>') {
-                      html = '';
-                  }
-                  ngModel.$setViewValue(html);
-              }
-          }
-      };
-  });
+]);
